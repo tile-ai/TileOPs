837b6500bff14532ff679343b022d5d680453ba5
Rachmanino <18805904201@163.com>
2025-06-09 18:13:11 +0800

    Run yapf and ruff

--- tla/kernel/__init__.py
+++ tla/kernel/__init__.py
@@ -2,5 +2,9 @@ from .mha import MHA_kernel
 from .mla import MLA_kernel
 from .gqa import GQA_kernel
 from .mamba_chunk_scan import MAMBA_CHUNK_SCAN_kernel
+from .linear_attention import FusedChunk_kernel
 
-__all__ = ["MHA_kernel", "MLA_kernel", "GQA_kernel", "MAMBA_CHUNK_SCAN_kernel"]
+__all__ = [
+    "MHA_kernel", "MLA_kernel", "GQA_kernel", "MAMBA_CHUNK_SCAN_kernel",
+    "FusedChunk_kernel"
+]
