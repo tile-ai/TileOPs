ffe01345fc3781d259f1b77c9a9782d6c20ce46b
xs-keju <93414213+xs-keju@users.noreply.github.com>
2025-06-09 17:54:15 +0800

    [Dev] Add mamba chunk scan attention fwd kernel (#5)
    * [Draft]Add mamba chunk scan attention fwd kernel
    
    * [Draft]Add mamba chunk scan attention fwd kernel
    
    * [Draft]Add mamba chunk scan attention fwd kernel
    
    * [Draft] add args.tune in mamba_chunk_scan test

--- tla/__init__.py
+++ tla/__init__.py
@@ -1,5 +1,6 @@
 from .kernel.mha import MHA_kernel
 from .kernel.mla import MLA_kernel
 from .kernel.gqa import GQA_kernel
+from .kernel.mamba_chunk_scan import MAMBA_CHUNK_SCAN_kernel
 
-__all__ = ["MLA_kernel", "GQA_kernel", "MHA_kernel"]
+__all__ = ["MHA_kernel", "MLA_kernel", "GQA_kernel", "MAMBA_CHUNK_SCAN_kernel"]
