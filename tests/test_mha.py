93d819bb28ecbe785e6cc79dd7a4f2f9eaaaacc3
Rachmanino <18805904201@163.com>
2025-06-11 15:41:29 +0800

    Add MHA fwd/bwd kernels&test for BSHD layout

---
+++ tests/test_mha.py
@@ -0,0 +1,24 @@
+import argparse
+from tla import MHA_kernel
+
+
+def test_mha_kernel(B, S, H, D , causal):
+    kernel = MHA_kernel(B, H, S, D, causal)
+    kernel.check()
+    kernel.profile()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--batch', type=int, default=8, help='batch size')
+    parser.add_argument('--seq_len',
+                        type=int,
+                        default=1024,
+                        help='sequence length')
+    parser.add_argument('--heads', type=int, default=32, help='num heads')
+    parser.add_argument('--dim', type=int, default=64, help='head dim')
+    parser.add_argument('--causal', action='store_true', default=False, help='causal attention')
+    args = parser.parse_args()
+    B, S, H, D, causal = args.batch, args.seq_len, args.heads, args.dim, args.causal
+
+    test_mha_kernel(B, S, H, D, causal)
