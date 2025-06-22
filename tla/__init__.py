822df322d552dab03e09cd4636d8e529f2e902b7
徐畅 <147292722+xuchangtolearn@users.noreply.github.com>
2025-06-22 20:37:43 +0800

    [Dev] Add bitnet prefill and decode kernels (#12)
    * [Draft]Add grouped query attention fwd and bwd kernels
    
    * remove profiling the performance from the check function
    
    * [Dev] Add bitnet prefill and decode kernels

--- tla/__init__.py
+++ tla/__init__.py
@@ -5,8 +5,9 @@ from .kernel.mamba_chunk_scan import MambaChunkScanKernel
 from .kernel.mamba_chunk_state import MambaChunkStateKernel
 from .kernel.blocksparse_attention import BlockSparseAttentionKernel
 from .kernel.linear_attention.fused_chunk import LinearAttentionFusedChunkKernel
+from .kernel.bitnet import Bitnet_158_int8xint2_kernel
 
 __all__ = [
     "MHAKernel", "MLAKernel", "GQAKernel", "MambaChunkScanKernel", "MambaChunkStateKernel",
-    "BlockSparseAttentionKernel", "LinearAttentionFusedChunkKernel"
+    "BlockSparseAttentionKernel", "LinearAttentionFusedChunkKernel", "Bitnet_158_int8xint2_kernel"
 ]
--- tla/kernel/__init__.py
+++ tla/kernel/__init__.py
@@ -5,8 +5,9 @@ from .mamba_chunk_scan import MambaChunkScanKernel
 from .mamba_chunk_state import MambaChunkStateKernel
 from .blocksparse_attention import BlockSparseAttentionKernel
 from .linear_attention.fused_chunk import LinearAttentionFusedChunkKernel
+from .bitnet import Bitnet_158_int8xint2_kernel
 
 __all__ = [
     "MHAKernel", "MLAKernel", "GQAKernel", "MambaChunkScanKernel", "MambaChunkStateKernel",
-    "BlockSparseAttentionKernel", "LinearAttentionFusedChunkKernel"
+    "BlockSparseAttentionKernel", "LinearAttentionFusedChunkKernel", "Bitnet_158_int8xint2_kernel"
 ]
