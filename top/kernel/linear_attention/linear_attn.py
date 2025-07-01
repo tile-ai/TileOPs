ad1ce0848d409a5afd790e1590dce5c78df1f853
Rachmanino <18805904201@163.com>
2025-07-01 11:44:04 +0800

    Fix lint

--- top/kernel/linear_attention/linear_attn.py
+++ top/kernel/linear_attention/linear_attn.py
@@ -8,10 +8,7 @@ import tilelang.language as T
 import fla.ops.linear_attn  # We compare with Triton implementation in FLA
 from top.utils import str2dtype, reduce_on_dim0, zero_pad
 
-__all__ = [
-    'LinearAttentionFusedChunkKernel',
-    'LinearAttentionFusedRecurrentKernel'
-]
+__all__ = ['LinearAttentionFusedChunkKernel', 'LinearAttentionFusedRecurrentKernel']
 
 
 @tl.jit(out_idx=[3])
@@ -229,12 +226,10 @@ class LinearAttentionFusedChunkKernel(nn.Module):
     def forward(self, q, k, v, scale=None):  # Layout: [B, S, H, D]
         if self.seq_len % self.chunk_size != 0:
             q, k, v = map(lambda x: zero_pad(x, self.chunk_size, 1), (q, k, v))
-        o = self.attention(q, k, v, scale, self.dtype, self.block_K, self.block_V,
-                              self.chunk_size)
+        o = self.attention(q, k, v, scale, self.dtype, self.block_K, self.block_V, self.chunk_size)
         if self.seq_len % self.chunk_size != 0:
             o = o[:, :self.seq_len, ...]  # Remove the padding
         return o
-
 
     @classmethod
     def ref_program(cls, q, k, v, scale=None):
@@ -284,16 +279,16 @@ def _fused_recurrent_fwd(B, H, S, D, scale=None, dtype='float16', BK=32, BV=32):
     accum_dtype = "float"
     NK = D // BK
     NV = D // BV
-        
+
     if scale is None:
         scale = D**-0.5
-        
+
     @T.prim_func
     def main(
-        Q: T.Tensor([B, H, S, D], dtype),  # type: ignore
-        K: T.Tensor([B, H, S, D], dtype),  # type: ignore
-        V: T.Tensor([B, H, S, D], dtype),  # type: ignore
-        Output: T.Tensor([NK, B, H, S, D], dtype)  # type: ignore
+            Q: T.Tensor([B, H, S, D], dtype),  # type: ignore
+            K: T.Tensor([B, H, S, D], dtype),  # type: ignore
+            V: T.Tensor([B, H, S, D], dtype),  # type: ignore
+            Output: T.Tensor([NK, B, H, S, D], dtype)  # type: ignore
     ):
         with T.Kernel(NV, NK, B * H) as (i_v, i_k, i_bh):
             i_b = i_bh // H
@@ -317,41 +312,42 @@ def _fused_recurrent_fwd(B, H, S, D, scale=None, dtype='float16', BK=32, BV=32):
                     h[i, j] += k[j] * v[i]
                     o[i, j] = h[i, j] * q[j]
                 T.reduce_sum(o, o_sum, dim=1, clear=True)
-                    
+
                 T.copy(o_sum, Output[i_k, i_b, i_h, t, i_v * BV:(i_v + 1) * BV])
-                
+
     return main
+
 
 @tl.jit(out_idx=[4, 5, 6])
 def _fused_recurrent_bwd(B, H, S, D, scale=None, dtype='float16', BK=32, BV=32):
     accum_dtype = "float"
     NK = D // BK
     NV = D // BV
-        
+
     if scale is None:
         scale = D**-0.5
-        
+
     @T.prim_func
     def main(
-        Q: T.Tensor([B, H, S, D], dtype),  # type: ignore
-        K: T.Tensor([B, H, S, D], dtype),  # type: ignore
-        V: T.Tensor([B, H, S, D], dtype),  # type: ignore
-        dO: T.Tensor([B, H, S, D], dtype),  # type: ignore
-        dQ: T.Tensor([NV, B, H, S, D], dtype),  # type: ignore
-        dK: T.Tensor([NV, B, H, S, D], dtype),  # type: ignore
-        dV: T.Tensor([NK, B, H, S, D], dtype)  # type: ignore
+            Q: T.Tensor([B, H, S, D], dtype),  # type: ignore
+            K: T.Tensor([B, H, S, D], dtype),  # type: ignore
+            V: T.Tensor([B, H, S, D], dtype),  # type: ignore
+            dO: T.Tensor([B, H, S, D], dtype),  # type: ignore
+            dQ: T.Tensor([NV, B, H, S, D], dtype),  # type: ignore
+            dK: T.Tensor([NV, B, H, S, D], dtype),  # type: ignore
+            dV: T.Tensor([NK, B, H, S, D], dtype)  # type: ignore
     ):
         with T.Kernel(NV, NK, B * H) as (i_v, i_k, i_bh):
             i_b = i_bh // H
             i_h = i_bh % H
-            
+
             q = T.alloc_shared([BK], accum_dtype)
             k = T.alloc_shared([BK], accum_dtype)
             v = T.alloc_shared([BV], accum_dtype)
             do = T.alloc_shared([BV], accum_dtype)
             dq = T.alloc_fragment([BK, BV], accum_dtype)
             dq_sum = T.alloc_fragment([BK], accum_dtype)
-            dk  = T.alloc_fragment([BK, BV], accum_dtype)
+            dk = T.alloc_fragment([BK, BV], accum_dtype)
             dk_sum = T.alloc_fragment([BK], accum_dtype)
             dv = T.alloc_fragment([BV, BK], accum_dtype)
             dv_sum = T.alloc_fragment([BV], accum_dtype)
@@ -359,7 +355,7 @@ def _fused_recurrent_bwd(B, H, S, D, scale=None, dtype='float16', BK=32, BV=32):
             dh = T.alloc_fragment([BK, BV], accum_dtype)
             T.clear(h)
             T.clear(dh)
-            
+
             # 1. Calculate dQ
             for t in T.Pipelined(0, S):
                 T.copy(K[i_b, i_h, t, i_k * BK:(i_k + 1) * BK], k)
@@ -372,16 +368,16 @@ def _fused_recurrent_bwd(B, H, S, D, scale=None, dtype='float16', BK=32, BV=32):
                 T.reduce_sum(dq, dq_sum, dim=1, clear=True)
                 for i in T.Parallel(BK):
                     dq_sum[i] *= scale
-                
+
                 T.copy(dq_sum, dQ[i_v, i_b, i_h, t, i_k * BK:(i_k + 1) * BK])
-                
+
             # 2. Calculate dK, dV (reversely)
             for t in T.Pipelined(0, S):
                 for i in T.Parallel(BK):
-                    q[i] = Q[i_b, i_h, S-t-1, i_k * BK + i] * scale
-                T.copy(K[i_b, i_h, S-t-1, i_k * BK:(i_k + 1) * BK], k)
-                T.copy(V[i_b, i_h, S-t-1, i_v * BV:(i_v + 1) * BV], v)
-                T.copy(dO[i_b, i_h, S-t-1, i_v * BV:(i_v + 1) * BV], do)
+                    q[i] = Q[i_b, i_h, S - t - 1, i_k * BK + i] * scale
+                T.copy(K[i_b, i_h, S - t - 1, i_k * BK:(i_k + 1) * BK], k)
+                T.copy(V[i_b, i_h, S - t - 1, i_v * BV:(i_v + 1) * BV], v)
+                T.copy(dO[i_b, i_h, S - t - 1, i_v * BV:(i_v + 1) * BV], do)
 
                 for i, j in T.Parallel(BK, BV):
                     dh[i, j] += q[i] * do[j]
@@ -390,11 +386,9 @@ def _fused_recurrent_bwd(B, H, S, D, scale=None, dtype='float16', BK=32, BV=32):
                 T.reduce_sum(dk, dk_sum, dim=1, clear=True)
                 T.reduce_sum(dv, dv_sum, dim=0, clear=True)
 
-                T.copy(dk_sum,
-                       dK[i_v, i_b, i_h, S-t-1, i_k * BK:(i_k + 1) * BK])
-                T.copy(dv_sum,
-                       dV[i_k, i_b, i_h, S-t-1, i_v * BV:(i_v + 1) * BV])
-                    
+                T.copy(dk_sum, dK[i_v, i_b, i_h, S - t - 1, i_k * BK:(i_k + 1) * BK])
+                T.copy(dv_sum, dV[i_k, i_b, i_h, S - t - 1, i_v * BV:(i_v + 1) * BV])
+
     return main
 
 
@@ -403,8 +397,8 @@ class _fused_recurrent_linear_attention(torch.autograd.Function):
     @staticmethod
     def forward(ctx, q, k, v, scale, dtype, BK, BV):
         B, H, S, D = q.shape
-        ctx.B, ctx.H, ctx.S, ctx.D, ctx.scale, ctx.dtype, ctx.BK, ctx.BV = (
-            B, H, S, D, scale, dtype, BK, BV)
+        ctx.B, ctx.H, ctx.S, ctx.D, ctx.scale, ctx.dtype, ctx.BK, ctx.BV = (B, H, S, D, scale,
+                                                                            dtype, BK, BV)
         ctx.save_for_backward(q, k, v)
 
         mod = _fused_recurrent_fwd(B, H, S, D, scale, dtype, BK, BV)
@@ -423,7 +417,7 @@ fused_recurrent_linear_attention = _fused_recurrent_linear_attention.apply
 
 class LinearAttentionFusedRecurrentKernel(nn.Module):
     '''Calculate the results in a recursive way.'''
-    
+
     def __init__(self,
                  batch_size,
                  seq_len,
@@ -452,7 +446,8 @@ class LinearAttentionFusedRecurrentKernel(nn.Module):
     @classmethod
     def ref_program(cls, q, k, v, scale=None):
         # You may need to comment out the deprecated warning in this function.
-        return fla.ops.linear_attn.fused_recurrent_linear_attn(q, k, v, scale, normalize=False, head_first=True)
+        return fla.ops.linear_attn.fused_recurrent_linear_attn(
+            q, k, v, scale, normalize=False, head_first=True)
 
     def gen_inputs(self, n: int = 4):
         return (torch.randn((self.batch_size, self.num_heads, self.seq_len, self.head_dim),
@@ -491,5 +486,3 @@ class LinearAttentionFusedRecurrentKernel(nn.Module):
         assert (dk - dk_ref).abs().mean() < 1e-4, "dk does not match reference"
         assert (dv - dv_ref).abs().mean() < 1e-4, "dv does not match reference"
         print("All checks passed! ✅")
-    
-
--- top/utils/utils.py
+++ top/utils/utils.py
@@ -1,12 +1,7 @@
 import torch
-from torch.nn import functional as F
 
 # A mapping from string dtype names to torch dtypes
-str2dtype = {
-    'float16': torch.float16,
-    'bfloat16': torch.bfloat16,
-    'float32': torch.float32
-}
+str2dtype = {'float16': torch.float16, 'bfloat16': torch.bfloat16, 'float32': torch.float32}
 
 # A mapping from torch dtypes to string names
 dtype2str = {v: k for k, v in str2dtype.items()}
@@ -33,15 +28,15 @@ def zero_pad(x: torch.Tensor, pad_size: int, dim: int) -> torch.Tensor:
         torch.Tensor: Padded tensor."""
     if x.size(dim) % pad_size == 0:
         return x
-    pad_len = (pad_size - x.size(dim) % pad_size) 
+    pad_len = (pad_size - x.size(dim) % pad_size)
     assert 0 < pad_len < pad_size
-    
+
     zero_shape = list(x.shape)
     zero_shape[dim] = pad_len
     zero_shape = tuple(zero_shape)
     zeros = torch.zeros(zero_shape, dtype=x.dtype, device=x.device)
     return torch.cat((x, zeros), dim=dim)
-    
+
 
 def ensure_contiguous(func: callable) -> callable:
     """Decorator to ensure that all tensor arguments are contiguous before calling the function.
@@ -49,8 +44,12 @@ def ensure_contiguous(func: callable) -> callable:
         func (callable): The function to decorate.
     Returns:
         callable: The decorated function."""
+
     def wrapper(*args, **kwargs):
         args = [arg.contiguous() if isinstance(arg, torch.Tensor) else arg for arg in args]
-        kwargs = {k: v.contiguous() if isinstance(v, torch.Tensor) else v for k, v in kwargs.items()}
+        kwargs = {
+            k: v.contiguous() if isinstance(v, torch.Tensor) else v for k, v in kwargs.items()
+        }
         return func(*args, **kwargs)
+
     return wrapper
