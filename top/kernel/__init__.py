626ef2b5ca8377330546150354fa5748c2035685
徐畅 <147292722+xuchangtolearn@users.noreply.github.com>
2025-07-02 15:37:33 +0800

    [Dev] Add GQA decode kernel and update naming (#20)
    * [Dev] Add GQA decode kernel and update naming
    
    * [Style] Format code
    
    * [Fix] Correct loop range of gqa_decode_split_ref using ceiling division

--- top/kernel/__init__.py
+++ top/kernel/__init__.py
@@ -3,7 +3,7 @@
 
 from .mha import MHAKernel, MHADecodeKernel
 from .mla import MLAKernel
-from .gqa import GQAKernel
+from .gqa import GQAKernel, GQADecodeKernel
 from .mamba_chunk_scan import MambaChunkScanKernel
 from .mamba_chunk_state import MambaChunkStateKernel
 from .blocksparse_attention import BlockSparseAttentionKernel
@@ -11,7 +11,8 @@ from .linear_attention.linear_attn import LinearAttentionFusedChunkKernel, Linea
 from .bitnet import Bitnet_158_int8xint2_kernel
 
 __all__ = [
-    "MHAKernel", "MHADecodeKernel", "MLAKernel", "GQAKernel", "MambaChunkScanKernel",
-    "MambaChunkStateKernel", "BlockSparseAttentionKernel", "LinearAttentionFusedChunkKernel",
-    "LinearAttentionFusedRecurrentKernel", "Bitnet_158_int8xint2_kernel"
+    "MHAKernel", "MHADecodeKernel", "MLAKernel", "GQAKernel", "GQADecodeKernel",
+    "MambaChunkScanKernel", "MambaChunkStateKernel", "BlockSparseAttentionKernel",
+    "LinearAttentionFusedChunkKernel", "LinearAttentionFusedRecurrentKernel",
+    "Bitnet_158_int8xint2_kernel"
 ]
