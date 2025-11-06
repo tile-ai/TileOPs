48def7b37cff5812b17ca398ff52e66f82aab820
RMLYC <472187190@qq.com>
2025-11-06 16:21:31 +0800

    remove sparse mla & mla to mla folder

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -2,3 +2,4 @@ from .benchmark import Benchmark  # noqa: F401
 from .flash_attn import *
 from .gemm import *
 from .flash_decode import *
+from .mla import *
--- benchmarks/flash_decode/__init__.py
+++ benchmarks/flash_decode/__init__.py
@@ -1,4 +1,2 @@
 from .mha_decode import *
 from .gqa_decode import *
-from .mla_decode import *
-from .sparse_mla import *
