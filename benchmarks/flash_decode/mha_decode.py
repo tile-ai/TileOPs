fix causal in mha decode

benchmarks/flash_decode/mha_decode.py
@@ -9,20 +9,19 @@ class mha_decode_benchmark(Benchmark):
 
     op_type = mha_decode
 
-    def __init__(self, batch, heads, seq_len_q, seq_len_kv, dim, is_causal, dtype):
+    def __init__(self, batch, heads, seq_len_q, seq_len_kv, dim, dtype):
         self.batch = batch
         self.heads = heads
         self.seq_len_q = seq_len_q
         self.seq_len_kv = seq_len_kv
         self.dim = dim
-        self.is_causal = is_causal
         self.dtype = dtype
 
     @property
     def total_flops(self):
         flops_per_matmul = 2.0 * self.batch * self.heads * self.seq_len_q * self.seq_len_kv * self.dim
         flops = flops_per_matmul * 2
-        return flops / 2 if self.is_causal else flops
+        return flops
 
     @property
     def total_memory(self):
@@ -46,6 +45,6 @@ class mha_decode_benchmark(Benchmark):
         k_bhsd = K.transpose(1, 2)   # [B, H, S_kv, D]
         v_bhsd = V.transpose(1, 2)   # [B, H, S_kv, D]
         with sdpa_kernel(backends=[SDPBackend.FLASH_ATTENTION]):
-            output_bhsd = F.scaled_dot_product_attention(q_bhsd, k_bhsd, v_bhsd, is_causal=self.is_causal)
+            output_bhsd = F.scaled_dot_product_attention(q_bhsd, k_bhsd, v_bhsd)
         output = output_bhsd.transpose(1, 2).contiguous()
         return output 

top/functions/mha_decode.py
@@ -26,7 +26,6 @@ class mha_decode_fn(Function):
                  seqlen_q,
                  seqlen_kv,
                  dim,
-                 is_causal,
                  dtype=torch.float16,
                  tune=False):
         self.batch = batch
@@ -34,11 +33,10 @@ class mha_decode_fn(Function):
         self.seqlen_q = seqlen_q
         self.seqlen_kv = seqlen_kv  
         self.dim = dim
-        self.is_causal = is_causal
 
         self.dtype = dtype
 
-        self.fwd_op = mha_decode(batch, heads, seqlen_q, seqlen_kv, dim, is_causal, dtype, tune=tune)
+        self.fwd_op = mha_decode(batch, heads, seqlen_q, seqlen_kv, dim, dtype, tune=tune)
 
     
     def forward(self, Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor) -> torch.Tensor:

top/ops/mha_decode.py
@@ -15,7 +15,6 @@ class mha_decode(Op):
                  seqlen_q,
                  seqlen_kv,
                  dim,
-                 is_causal,
                  dtype=torch.float16,
                  kernel_map: Optional[Dict[str, Kernel]] = None,
                  tune=False):
@@ -24,13 +23,12 @@ class mha_decode(Op):
         self.seqlen_q = seqlen_q
         self.seqlen_kv = seqlen_kv
         self.dim = dim
-        self.is_causal = is_causal
 
         self.dtype = dtype
 
         self.dispatch_kernel(kernel_map)
         self.kernel = self.kernel_map["mha_decode_kernel"](
-            batch, heads, seqlen_q, seqlen_kv, dim, is_causal, self.dtype, tune=tune)
+            batch, heads, seqlen_q, seqlen_kv, dim, False, self.dtype, tune=tune)
 
     @property
     def default_kernel_map(self):