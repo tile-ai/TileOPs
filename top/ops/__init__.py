1228055e086de7896a504e1b6e3089b5ef5c618c
jieneng.yu <1033160740@qq.com>
2026-01-08 21:58:52 +0800

    refector

--- top/ops/__init__.py
+++ top/ops/__init__.py
@@ -1,12 +1,12 @@
 from .deepseek_dsa_decode import DeepSeekSparseAttentionDecodeWithKVCacheOp
 from .deepseek_mla_decode import MultiHeadLatentAttentionDecodeWithKVCacheOp
+from .deepseek_nsa import MeanPoolingForwardOp
 from .gemm import GemmOp
 from .gqa import GroupQueryAttentionBwdOp, GroupQueryAttentionFwdOp
 from .gqa_decode import GroupQueryAttentionDecodeWithKVCacheOp
 from .mha import MultiHeadAttentionBwdOp, MultiHeadAttentionFwdOp
 from .mha_decode import MultiHeadAttentionDecodeWithKVCacheOp
 from .op import Op  # noqa: F401
-from .deepseek_nsa import MeanPoolingForwardOp
 
 __all__ = [
     "Op",
