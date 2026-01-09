1a089077e444b10f1d76d51246f9f4be5cc9e314
RMLYC <472187190@qq.com>
2026-01-09 12:00:57 +0800

    fix code format

--- top/functions/function.py
+++ top/functions/function.py
@@ -7,10 +7,7 @@ import torch
 class Function(ABC):
 
     @abstractmethod
-    def forward(
-            self,
-            *args: object,  # noqa: U100
-            **kwargs: object) -> Union[torch.Tensor, Tuple]:  # noqa: U100
+    def forward(self, *args: object, **kwargs: object) -> Union[torch.Tensor, Tuple]:  # noqa: U100
         raise NotImplementedError
 
     def __call__(self, *args: object, **kwargs: object) -> Union[torch.Tensor, Tuple]:
