f3e09afb3a64a3e11c96dd05b453f040d6fbe677
RMLYC <472187190@qq.com>
2026-01-09 14:26:16 +0800

    add refactor

--- benchmarks/flash_attn/gqa.py
+++ benchmarks/flash_attn/gqa.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional, Tuple
+from typing import Optional, Tuple, Union
 
 import flash_attn_interface
 import torch
@@ -31,20 +31,30 @@ class GroupQueryAttentionFwdBenchmark(Benchmark):
 
     @property
     def total_memory(self) -> int:
-        return 2 * self.batch * self.seq_len * self.dim * (self.heads +
-                                                           self.heads_kv) * self.dtype.itemsize
+        query_size = self.batch * self.seq_len * self.heads * self.dim
+        kv_size = self.batch * self.seq_len * self.heads_kv * self.dim
+        return 2 * (query_size + kv_size) * self.dtype.itemsize
 
     def gen_inputs(self) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        Q = torch.randn(
-            self.batch, self.seq_len, self.heads, self.dim, device='cuda',
-            dtype=self.dtype).contiguous()
-        K = torch.randn(
-            self.batch, self.seq_len, self.heads_kv, self.dim, device='cuda',
-            dtype=self.dtype).contiguous()
-        V = torch.randn(
-            self.batch, self.seq_len, self.heads_kv, self.dim, device='cuda',
-            dtype=self.dtype).contiguous()
-        return Q, K, V
+        q = torch.randn(self.batch,
+                        self.seq_len,
+                        self.heads,
+                        self.dim,
+                        device='cuda',
+                        dtype=self.dtype).contiguous()
+        k = torch.randn(self.batch,
+                        self.seq_len,
+                        self.heads_kv,
+                        self.dim,
+                        device='cuda',
+                        dtype=self.dtype).contiguous()
+        v = torch.randn(self.batch,
+                        self.seq_len,
+                        self.heads_kv,
+                        self.dim,
+                        device='cuda',
+                        dtype=self.dtype).contiguous()
+        return q, k, v
 
     def ref_program(self, q: torch.Tensor, k: torch.Tensor,
                     v: torch.Tensor) -> Tuple[torch.Tensor, Optional[torch.Tensor]]:
@@ -52,8 +62,11 @@ class GroupQueryAttentionFwdBenchmark(Benchmark):
         k_bhsd = k.transpose(1, 2)
         v_bhsd = v.transpose(1, 2)
         with sdpa_kernel(backends=[SDPBackend.FLASH_ATTENTION]):
-            output_bhsd = F.scaled_dot_product_attention(
-                q_bhsd, k_bhsd, v_bhsd, is_causal=self.is_causal, enable_gqa=True)
+            output_bhsd = F.scaled_dot_product_attention(q_bhsd,
+                                                         k_bhsd,
+                                                         v_bhsd,
+                                                         is_causal=self.is_causal,
+                                                         enable_gqa=True)
         output = output_bhsd.transpose(1, 2).contiguous()
         return output, None  # do not check lse
 
@@ -69,17 +82,21 @@ class GroupQueryAttentionFwdBenchmark(Benchmark):
 
         # Be robust to different return types.
         if isinstance(out, tuple):
-            out = out[0]
+            return out[0]
 
         return out
 
     def baseline_profile(self,
-                         *inputs: Any,
+                         *inputs: Union[torch.Tensor, Tuple],
                          warmup: int = 100,
                          rep: int = 10,
-                         device: str = "cuda:0") -> Any:
-        return super().baseline_profile(
-            self.baseline_program, *inputs, backend="FA3", warmup=warmup, rep=rep, device=device)
+                         device: str = "cuda:0") -> None:
+        return super().baseline_profile(self.baseline_program,
+                                        *inputs,
+                                        backend="FA3",
+                                        warmup=warmup,
+                                        rep=rep,
+                                        device=device)
 
 
 class GroupQueryAttentionBwdBenchmark(Benchmark):
@@ -104,83 +121,98 @@ class GroupQueryAttentionBwdBenchmark(Benchmark):
 
     @property
     def total_memory(self) -> int:
-        return self.batch * (3 * self.heads +
-                             4 * self.heads_kv) * self.seq_len * self.dim * self.dtype.itemsize
+        total_heads = (3 * self.heads + 4 * self.heads_kv)
+        return self.batch * total_heads * self.seq_len * self.dim * self.dtype.itemsize
 
     def gen_inputs(
         self
     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-        Q = torch.randn(
-            self.batch,
-            self.seq_len,
-            self.heads,
-            self.dim,
-            dtype=self.dtype,
-            device='cuda',
-            requires_grad=True)
-        K = torch.randn(
-            self.batch,
-            self.seq_len,
-            self.heads_kv,
-            self.dim,
-            dtype=self.dtype,
-            device='cuda',
-            requires_grad=True)
-        V = torch.randn(
-            self.batch,
-            self.seq_len,
-            self.heads_kv,
-            self.dim,
-            dtype=self.dtype,
-            device='cuda',
-            requires_grad=True)
-        dO = torch.randn(
-            self.batch, self.seq_len, self.heads, self.dim, dtype=self.dtype, device='cuda')
+        q = torch.randn(self.batch,
+                        self.seq_len,
+                        self.heads,
+                        self.dim,
+                        dtype=self.dtype,
+                        device='cuda',
+                        requires_grad=True)
+        k = torch.randn(self.batch,
+                        self.seq_len,
+                        self.heads_kv,
+                        self.dim,
+                        dtype=self.dtype,
+                        device='cuda',
+                        requires_grad=True)
+        v = torch.randn(self.batch,
+                        self.seq_len,
+                        self.heads_kv,
+                        self.dim,
+                        dtype=self.dtype,
+                        device='cuda',
+                        requires_grad=True)
+        grad_output = torch.randn(self.batch,
+                                  self.seq_len,
+                                  self.heads,
+                                  self.dim,
+                                  dtype=self.dtype,
+                                  device='cuda')
 
         fwd_op = GroupQueryAttentionFwdOp(self.batch, self.heads, self.heads_kv, self.seq_len,
                                           self.dim, self.is_causal, self.dtype)
         with torch.no_grad():
-            O, lse = fwd_op(Q, K, V)
+            o, lse = fwd_op(q, k, v)
 
-        return Q, K, V, O, dO, lse
+        return q, k, v, o, grad_output, lse
 
-    def ref_program(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, o: torch.Tensor,
-                    do: torch.Tensor,
-                    lse: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    def ref_program(
+        self,
+        q: torch.Tensor,
+        k: torch.Tensor,
+        v: torch.Tensor,
+        grad_output: torch.Tensor,
+        o: torch.Tensor,  # noqa: U100
+        lse: torch.Tensor  # noqa: U100
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:  # noqa: U100
         q_bhsd = q.transpose(1, 2)  # [B, H, S, D]
         k_bhsd = k.transpose(1, 2)
         v_bhsd = v.transpose(1, 2)
         with sdpa_kernel(backends=[SDPBackend.FLASH_ATTENTION]):
-            output_bhsd = F.scaled_dot_product_attention(
-                q_bhsd, k_bhsd, v_bhsd, is_causal=self.is_causal, enable_gqa=True)
+            output_bhsd = F.scaled_dot_product_attention(q_bhsd,
+                                                         k_bhsd,
+                                                         v_bhsd,
+                                                         is_causal=self.is_causal,
+                                                         enable_gqa=True)
         output = output_bhsd.transpose(1, 2).contiguous()
 
-        output.backward(do)
+        output.backward(grad_output)
         return q.grad, k.grad, v.grad
 
     def baseline_program(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, o: torch.Tensor,
-                         do: torch.Tensor,
+                         grad_output: torch.Tensor,
                          lse: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
 
         softmax_scale = q.shape[-1]**(-0.5)
 
-        dQ = torch.empty_like(q)
-        dK = torch.empty_like(k)
-        dV = torch.empty_like(v)
-        dQ, dK, dV, _ = flash_attn_interface._flash_attn_backward(do, q, k, v, o, lse, None, None,
-                                                                  None, None, None, None, dQ, dK,
-                                                                  dV, softmax_scale, self.is_causal)
-        return dQ, dK, dV
+        dq = torch.empty_like(q)
+        dk = torch.empty_like(k)
+        dv = torch.empty_like(v)
+        dq, dk, dv, _ = flash_attn_interface._flash_attn_backward(grad_output, q, k, v, o, lse,
+                                                                  None, None, None, None, None,
+                                                                  None, dq, dk, dv, softmax_scale,
+                                                                  self.is_causal)
+        return dq, dk, dv
 
     def baseline_profile(self,
-                         *inputs: Any,
+                         *inputs: Union[torch.Tensor, Tuple],
                          warmup: int = 100,
                          rep: int = 10,
-                         device: str = "cuda:0") -> Any:
+                         device: str = "cuda:0") -> None:
 
         print("===== Profiling GQA FA3 backend =====")
-        return super().baseline_profile(
-            self.baseline_program, *inputs, backend="FA3", warmup=warmup, rep=rep, device=device)
+        return super().baseline_profile(self.baseline_program,
+                                        *inputs,
+                                        backend="FA3",
+                                        warmup=warmup,
+                                        rep=rep,
+                                        device=device)
 
 
 class GroupQueryAttentionBenchmark(Benchmark):
@@ -218,17 +250,17 @@ class GroupQueryAttentionBenchmark(Benchmark):
 
     def gen_inputs(self) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         if self.grad:
-            Q, K, V, _, _, _ = self.gqa_bwd_bench.gen_inputs()
-            return Q, K, V
-        else:
-            return self.gqa_fwd_bench.gen_inputs()
+            q, k, v, _, _, _ = self.gqa_bwd_bench.gen_inputs()
+            return q, k, v
+        return self.gqa_fwd_bench.gen_inputs()
 
-    def ref_program(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor) -> Any:
+    def ref_program(self, q: torch.Tensor, k: torch.Tensor,
+                    v: torch.Tensor) -> Union[torch.Tensor, tuple]:
 
         output = self.gqa_fwd_bench.ref_program(q, k, v)[0]
         if not self.grad:
             return output
-        else:
-            loss = output.sum()
-            loss.backward()
-            return output, q.grad, k.grad, v.grad
+
+        loss = output.sum()
+        loss.backward()
+        return output, q.grad, k.grad, v.grad
