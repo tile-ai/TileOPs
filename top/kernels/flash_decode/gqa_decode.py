0ed69f45db53473ccbea11e3a790a2c6831bc3a5
Ang Gao <gaoang0125@163.com>
2026-01-14 16:54:04 +0800

    [BugFix] Support varying length and assigning data type in MHA_decode and GQA_decode kernels (#101)
    This PR fixes the following bugs:
    
    - 1. resolve #89 
    
    These two kernels can now perform decoding with arbitrary KV cache
    lengths.
    
    - 2. resolve #78 
    
    A padding mechanism has been added to avoid repeatedly initializing and
    compiling the kernel as the KV cache grows incrementally. However, the
    maximum KV cache length must still be specified at startup.
    
    - 3. resolve #90 
    
    The input variables have been rearranged to support multiple data types
    in the kernels. Both float16 and bfloat16 are now supported.
    
    Moreover, tests for sequence length and data types have been added to
    /tests/ops/.

--- top/kernels/flash_decode/gqa_decode.py
+++ top/kernels/flash_decode/gqa_decode.py
@@ -1,22 +1,19 @@
-import itertools
-from typing import Optional
-
+import torch
 import tilelang
 import tilelang.language as T
-import torch
-
 from top.kernels.kernel import Kernel
+from typing import Optional
+import itertools
 
 __all__ = ["gqa_decode_kernel"]
 
 
-def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim):
+def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim, dtype):
     scale = (1.0 / dim)**0.5 * 1.44269504  # log2(e)
-    dtype = "float16"
     accum_dtype = "float"
 
     @tilelang.jit(
-        out_idx=[5],
+        out_idx=[-1],
         pass_configs={
             tilelang.PassConfigKey.TL_ENABLE_FAST_MATH: True,
         },
@@ -38,10 +35,13 @@ def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim):
                 Q: T.Tensor(shape_q, dtype),
                 K: T.Tensor(shape_k, dtype),
                 V: T.Tensor(shape_v, dtype),
+                real_seqlen_kv: T.int32,
                 Output: T.Tensor([batch, heads, dim], dtype),
+
         ):
             with T.Kernel(
                     batch, heads // valid_block_H, num_split, threads=threads) as (bx, by, bz):
+
                 Q_shared = T.alloc_shared([block_H, dim], dtype)
                 K_shared = T.alloc_shared([block_N, dim], dtype)
                 V_shared = T.alloc_shared([block_N, dim], dtype)
@@ -64,7 +64,7 @@ def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim):
                 T.fill(logsum, 0)
                 T.fill(scores_max, -T.infinity(accum_dtype))
 
-                loop_range = T.ceildiv((seqlen_kv // num_split), block_N)
+                loop_range = T.ceildiv(real_seqlen_kv, block_N)
                 for k in T.Pipelined(loop_range, num_stages=num_stages):
                     T.copy(K[bid, k * block_N:(k + 1) * block_N, cur_kv_head, :], K_shared)
                     T.clear(acc_s)
@@ -74,6 +74,9 @@ def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim):
                         acc_s,
                         transpose_B=True,
                         policy=T.GemmWarpPolicy.FullRow)
+                    for i, j in T.Parallel(block_H, block_N):
+                        acc_s[i, j] = T.if_then_else((k*block_N+j<real_seqlen_kv), acc_s[i, j],
+                                                     -T.infinity(accum_dtype))
                     T.copy(scores_max, scores_max_prev)
                     T.fill(scores_max, -T.infinity(accum_dtype))
                     T.reduce_max(acc_s, scores_max, dim=1, clear=False)
@@ -93,6 +96,7 @@ def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim):
                     acc_o[i, j] /= logsum[i]
                 for i in T.Parallel(block_H):
                     logsum[i] = T.log2(logsum[i]) + scores_max[i] * scale
+
                 T.copy(acc_o[:valid_block_H, :], O_shared)
                 T.copy(O_shared, Output[bid, hid * valid_block_H:(hid + 1) * valid_block_H, :])
 
@@ -101,8 +105,10 @@ def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim):
                 Q: T.Tensor(shape_q, dtype),
                 K: T.Tensor(shape_k, dtype),
                 V: T.Tensor(shape_v, dtype),
+                real_seqlen_kv: T.int32,
                 glse: T.Tensor([batch, heads, num_split], dtype),
                 Output_partial: T.Tensor(part_shape, dtype),
+                split_length: T.Tensor(num_split, "int32"),
         ):
             with T.Kernel(
                     batch, heads // valid_block_H, num_split, threads=threads) as (bx, by, bz):
@@ -119,6 +125,12 @@ def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim):
                 scores_sum = T.alloc_fragment([block_H], accum_dtype)
                 logsum = T.alloc_fragment([block_H], accum_dtype)
 
+
+                split_length_shared = T.alloc_shared([num_split], "int32")
+                T.copy(split_length,split_length_shared, disable_tma=True)
+
+                seqlen_kv=real_seqlen_kv
+
                 bid = bx
                 hid = by
                 sid = bz
@@ -129,12 +141,13 @@ def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim):
                 T.fill(logsum, 0)
                 T.fill(scores_max, -T.infinity(accum_dtype))
 
-                loop_range = T.ceildiv((seqlen_kv // num_split), valid_block_N)
+                # loop_range = T.ceildiv((seqlen_kv // num_split), valid_block_N)
+                loop_range = T.ceildiv(split_length_shared[sid], block_N)
 
                 for k in T.Pipelined(loop_range, num_stages=num_stages):
                     T.copy(
-                        K[bid, (seqlen_kv // num_split) * sid +
-                          k * valid_block_N:(seqlen_kv // num_split) * sid +
+                        K[bid, (seqlen_kv // (num_split * block_N) * block_N) * sid +
+                          k * valid_block_N:(seqlen_kv // (num_split * block_N) * block_N) * sid +
                           (k + 1) * valid_block_N, cur_kv_head, :], K_shared)
                     T.clear(acc_s)
                     T.gemm(
@@ -143,6 +156,10 @@ def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim):
                         acc_s,
                         transpose_B=True,
                         policy=T.GemmWarpPolicy.FullRow)
+                    for i, j in T.Parallel(block_H, valid_block_N):
+                        acc_s[i, j] = T.if_then_else(
+                            (k * block_N + j < split_length[sid]), acc_s[i, j],
+                            -T.infinity(accum_dtype))
                     T.copy(scores_max, scores_max_prev)
                     T.fill(scores_max, -T.infinity(accum_dtype))
                     T.reduce_max(acc_s, scores_max, dim=1, clear=False)
@@ -157,8 +174,8 @@ def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim):
                     for i, j in T.Parallel(block_H, dim):
                         acc_o[i, j] *= scores_scale[i]
                     T.copy(
-                        V[bid, (seqlen_kv // num_split) * sid +
-                          k * valid_block_N:(seqlen_kv // num_split) * sid +
+                        V[bid, (seqlen_kv // (num_split * block_N) * block_N) * sid +
+                          k * valid_block_N:(seqlen_kv // (num_split * block_N) * block_N) * sid +
                           (k + 1) * valid_block_N, cur_kv_head, :], V_shared)
                     T.gemm(acc_s_cast, V_shared, acc_o, policy=T.GemmWarpPolicy.FullRow)
                 for i, j in T.Parallel(block_H, dim):
@@ -180,7 +197,7 @@ def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim):
                 Output: T.Tensor(shape_o, dtype),
         ):
             with T.Kernel(heads, batch, threads=128) as (by, bz):
-                # 1) 读 glse 到一个 1D fragment，再做 reduce_max
+                #
                 glse_vec = T.alloc_fragment([num_split], dtype)
                 for k in T.Parallel(num_split):
                     glse_vec[k] = glse[bz, by, k]
@@ -188,14 +205,14 @@ def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim):
                 T.fill(lse_max, -T.infinity(accum_dtype))
                 T.reduce_max(glse_vec, lse_max, dim=0, clear=False)
 
-                # 2) 计算 logsum（串行或小批并行累加）
+                #
                 lse_logsum = T.alloc_local([1], accum_dtype)
                 lse_logsum[0] = 0
                 for k in T.serial(num_split):
                     lse_logsum[0] += T.exp2(glse[bz, by, k] - lse_max[0])
                 lse_logsum[0] = T.log2(lse_logsum[0]) + lse_max[0]
 
-                # 3) 按权重合并 partial 输出
+                #
                 o_accum = T.alloc_fragment([dim], accum_dtype)
                 T.clear(o_accum)
                 for k in T.serial(num_split):
@@ -210,11 +227,14 @@ def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim):
                 Q: T.Tensor(shape_q, dtype),
                 K: T.Tensor(shape_k, dtype),
                 V: T.Tensor(shape_v, dtype),
+                real_seqlen_kv: T.int32,
                 glse: T.Tensor([batch, heads, num_split], dtype),
                 Output_partial: T.Tensor(part_shape, dtype),
+                split_length: T.Tensor(num_split, "int32"),
                 Output: T.Tensor(shape_o, dtype),
+
         ):
-            _gqa_decode_split(Q, K, V, glse, Output_partial)
+            _gqa_decode_split(Q, K, V,real_seqlen_kv,  glse, Output_partial,split_length)
             combine(glse, Output_partial, Output)
 
         @T.prim_func
@@ -222,11 +242,10 @@ def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim):
                 Q: T.Tensor(shape_q, dtype),
                 K: T.Tensor(shape_k, dtype),
                 V: T.Tensor(shape_v, dtype),
-                glse: T.Tensor([batch, heads, num_split], dtype),
-                Output_partial: T.Tensor(part_shape, dtype),
+                real_seqlen_kv: T.int32,
                 Output: T.Tensor(shape_o, dtype),
         ):
-            _gqa_decode_no_split(Q, K, V, Output)
+            _gqa_decode_no_split(Q, K, V, real_seqlen_kv, Output)
 
         if num_split > 1:
             return gqa_decode_split
@@ -237,13 +256,30 @@ def _gqa_decode_kernel(batch, heads, groups, seqlen_kv, dim):
 
 
 @torch.library.custom_op("top::gqa_decode_wrapped_kernel", mutates_args=())
-def _gqa_decode_wrapped_kernel(batch: int, heads: int, groups: int, seqlen_kv: int, dim: int,
+def _gqa_decode_wrapped_kernel(batch: int, heads: int, groups: int, seqlen_kv: int, real_seqlen_kv:int, dim: int, dtype:str,
                                block_H: int, block_N: int, num_stages: int, threads: int,
                                num_split: int, Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor,
-                               glse: torch.Tensor, Output_partial: torch.Tensor) -> torch.Tensor:
+                                glse: torch.Tensor,
+                               Output_partial: torch.Tensor) -> torch.Tensor:
+
+    assert K.shape[1] == V.shape[1] == seqlen_kv , "error: dimension mismatch!"
+    assert real_seqlen_kv <= seqlen_kv , "error: seqlen_kv mismatch!"
+    split_length = torch.zeros(num_split, dtype=torch.int32, device=Q.device)
+    for k in range(num_split):
+        split_length[k] = real_seqlen_kv // (num_split * block_N) * block_N
+    split_length[-1] = real_seqlen_kv - (num_split - 1) * (real_seqlen_kv // (num_split * block_N) * block_N)
+
+
+    if (split_length[0] == 0):
+        num_split = 1
+    if (num_split == 1):
+        return _gqa_decode_kernel(batch, heads, groups, seqlen_kv,
+                                  dim, dtype)(block_H, block_N, num_split, num_stages,
+                                       threads)(Q, K, V, real_seqlen_kv)
+
     return _gqa_decode_kernel(batch, heads, groups, seqlen_kv,
-                              dim)(block_H, block_N, num_split, num_stages, threads)(Q, K, V, glse,
-                                                                                     Output_partial)
+                              dim, dtype)(block_H, block_N, num_split, num_stages,
+                                   threads)(Q, K, V, real_seqlen_kv, glse, Output_partial,split_length)
 
 
 @_gqa_decode_wrapped_kernel.register_fake
@@ -284,7 +320,7 @@ class gqa_decode_kernel(Kernel):
         self.dtype = dtype
 
         self.kernel = _gqa_decode_kernel(self.batch, self.heads, self.groups, self.seqlen_kv,
-                                         self.dim)
+                                         self.dim, self.dtype_str)
 
         self.init_config(config, tune)
 
@@ -310,14 +346,15 @@ class gqa_decode_kernel(Kernel):
         } for c in _configs]
         return configs
 
-    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor):
+    def forward(self, Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor, real_seqlen_kv: int):
         glse = torch.empty((self.batch, self.heads, self.config["num_split"]),
                            dtype=self.dtype,
-                           device=q.device)
+                           device=Q.device)
         Output_partial = torch.empty((self.batch, self.heads, self.config["num_split"], self.dim),
                                      dtype=self.dtype,
-                                     device=q.device)
-        return _gqa_decode_wrapped_kernel(self.batch, self.heads, self.groups, self.seqlen_kv,
-                                          self.dim, self.config["block_H"], self.config["block_N"],
+                                     device=Q.device)
+        return _gqa_decode_wrapped_kernel(self.batch, self.heads, self.groups, self.seqlen_kv, real_seqlen_kv,
+                                          self.dim, self.dtype_str,self.config["block_H"], self.config["block_N"],
                                           self.config["num_stages"], self.config["threads"],
-                                          self.config["num_split"], q, k, v, glse, Output_partial)
+                                          self.config["num_split"], Q, K, V, glse,
+                                          Output_partial)
