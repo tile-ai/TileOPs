51f105002c58a7dd2680d289f679f5f29c6099ac
lcy-seso <lcy.seso@gmail.com>
2026-01-23 09:41:15 +0800

    chore: fix pre-commit checks.

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -12,7 +12,10 @@ from .flash_attn import (
     MultiHeadAttentionBwdBenchmark,
     MultiHeadAttentionFwdBenchmark,
 )
-from .flash_decode import GroupQueryAttentionDecodeBenchmark, MultiHeadAttentionDecodeBenchmark
+from .flash_decode import (
+    GroupQueryAttentionDecodeBenchmark,
+    MultiHeadAttentionDecodeBenchmark,
+)
 from .gemm import GemmBenchmark, MatMulBenchmark
 from .grouped_gemm import (
     GroupedGemmBenchmark,
@@ -23,23 +26,22 @@ from .grouped_gemm import (
 )
 
 __all__ = [
-    'Benchmark',
-    'MultiHeadAttentionBenchmark',
-    'MultiHeadAttentionBwdBenchmark',
-    'MultiHeadAttentionFwdBenchmark',
-    'GroupQueryAttentionBenchmark',
-    'GroupQueryAttentionFwdBenchmark',
-    'GroupQueryAttentionBwdBenchmark',
-    'GemmBenchmark',
-    'MultiHeadAttentionDecodeBenchmark',
-    'GroupQueryAttentionDecodeBenchmark',
-    'MultiHeadLatentAttentionDecodeBenchmark',
-    'DeepSeekSparseAttentionDecodeBenchmark',
-    'MatMulBenchmark',
+    "Benchmark",
+    "MultiHeadAttentionBenchmark",
+    "MultiHeadAttentionBwdBenchmark",
+    "MultiHeadAttentionFwdBenchmark",
+    "GroupQueryAttentionBenchmark",
+    "GroupQueryAttentionFwdBenchmark",
+    "GroupQueryAttentionBwdBenchmark",
+    "GemmBenchmark",
+    "MultiHeadAttentionDecodeBenchmark",
+    "GroupQueryAttentionDecodeBenchmark",
+    "MultiHeadLatentAttentionDecodeBenchmark",
+    "DeepSeekSparseAttentionDecodeBenchmark",
+    "MatMulBenchmark",
     "GroupedGemmBenchmark",
     "GroupedGemmNTBenchmark",
     "GroupedGemmNNBenchmark",
     "GroupedGemmTNBenchmark",
     "GroupedGemmTTBenchmark",
-    "Fp8LightingIndexerBenchmark",
 ]
