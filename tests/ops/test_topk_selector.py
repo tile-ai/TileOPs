fe09bd32ff2c5cc98ad1c617aaee0a462bb42e13
stelladuyx <u3605151@connect.hku.hk>
2026-01-26 10:53:32 +0800

    use pytest for topk test

--- tests/ops/test_topk_selector.py
+++ tests/ops/test_topk_selector.py
@@ -1,41 +1,28 @@
-import argparse
-
-import torch
-
+import pytest
 from benchmarks import TopkSelectorBenchmark
 from top.ops import TopkSelectorOp
 from top.utils import str2dtype
 
 
-def test_topk_selector(batch: int,
-                       seq_len: int,
-                       topk: int,
-                       in_dtype: torch.dtype,
-                       out_dtype: torch.dtype,
-                       tune: bool = False) -> None:
+@pytest.mark.parametrize("batch, seq_len, topk, in_dtype, out_dtype, tune", [
+    (64, 32 * 1024, 1024, "float32", "int32", False),
+    (64, 32 * 1024, 2048, "float32", "int32", False),
+    (128, 64 * 1024, 1024, "float32", "int32", False),
+    (128, 64 * 1024, 2048, "float32", "int32", False),
+])
+def test_topk_selector_op(batch: int, seq_len: int, topk: int, in_dtype: str, out_dtype: str,
+                          tune: bool) -> None:
+    in_dtype = str2dtype[in_dtype]
+    out_dtype = str2dtype[out_dtype]
+
     op = TopkSelectorOp(batch, seq_len, topk, in_dtype, out_dtype, tune=tune)
-    benchmark = TopkSelectorBenchmark(
-        batch,
-        seq_len,
-        topk,
-        in_dtype,
-        out_dtype,
-    )
+    benchmark = TopkSelectorBenchmark(batch, seq_len, topk, in_dtype, out_dtype)
 
     inputs = benchmark.gen_inputs()
+
     benchmark.check(op, *inputs)
     benchmark.profile(op, *inputs)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--batch', type=int, default=64, help='batch size')
-    parser.add_argument('--seq_len', type=int, default=32 * 1024, help='sequence length')
-    parser.add_argument('--topk', type=int, default=2048, help='topk')
-    parser.add_argument('--in_dtype', type=str, default="float32", help='input type')
-    parser.add_argument('--out_dtype', type=str, default="int32", help='output type')
-    parser.add_argument('--tune', action='store_true', default=False, help='enable autotune')
-    args = parser.parse_args()
-
-    test_topk_selector(args.batch, args.seq_len, args.topk, str2dtype[args.in_dtype],
-                       str2dtype[args.out_dtype], args.tune)
+    pytest.main()
