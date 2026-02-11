43bc32144a08b1bfdce5543c31216641205370bd
RMLYC <472187190@qq.com>
2026-02-11 11:16:10 +0800

    add gemv

---
+++ tests/ops/test_gemv.py
@@ -0,0 +1,28 @@
+import torch
+import pytest
+
+from benchmarks import GemvBenchmark
+from top.ops import GemvOp
+
+
+@pytest.mark.parametrize(
+    "n, k, dtype, tune",
+    [
+        (1024, 1024, torch.float16, False),
+        (64, 512, torch.float16, False),
+        (64, 64, torch.float16, False),
+    ],
+)
+def test_gemv(n: int, k: int, dtype: torch.dtype, tune: bool) -> None:
+    op = GemvOp(n, k, dtype=dtype, tune=tune)
+    benchmark = GemvBenchmark(n, k, dtype)
+
+    inputs = benchmark.gen_inputs()
+
+    benchmark.check(op, *inputs, atol=1e-5, rtol=1e-3)
+    benchmark.profile(op, *inputs)
+
+
+if __name__ == "__main__":
+    # Run tests with pytest
+    pytest.main([__file__, "-vvs"])
