6b6a76bae53955319b25bd2b9d0b2b12d59e20a4
RMLYC <472187190@qq.com>
2026-03-05 16:33:20 +0800

    [Refactor][INSTANCE_NORM] move kernel module to norm directory

--- tileops/ops/instance_norm.py
+++ tileops/ops/instance_norm.py
@@ -2,7 +2,7 @@ from typing import Dict, Optional
 
 import torch
 
-from tileops.kernels.instance_norm import InstanceNormKernel
+from tileops.kernels.norm import InstanceNormKernel
 from tileops.kernels.kernel import Kernel
 
 from .op import Op
