6d8be4d2a4f44fda49c8ef502b6fe4ce3e071eef
RMLYC <472187190@qq.com>
2026-04-07 14:44:53 +0800

    [FEAT][POOL] add max_pool2d operator

--- tileops/kernels/pool/__init__.py
+++ tileops/kernels/pool/__init__.py
@@ -1,5 +1,6 @@
 from .avg_pool1d import AvgPool1dKernel
 from .avg_pool2d import AvgPool2dKernel
 from .avg_pool3d import AvgPool3dKernel
+from .max_pool2d import MaxPool2dKernel
 
-__all__ = ["AvgPool1dKernel", "AvgPool2dKernel", "AvgPool3dKernel"]
+__all__ = ["AvgPool1dKernel", "AvgPool2dKernel", "AvgPool3dKernel", "MaxPool2dKernel"]
