7b7b52e0d9cb801653aa10015573f34de0d5b27a
jieneng.yu <1033160740@qq.com>
2026-04-27 18:20:47 +0800

    [Feat][MoE] export MoE ABC classes and concrete implementations

--- tileops/ops/moe/__init__.py
+++ tileops/ops/moe/__init__.py
@@ -1,5 +1,15 @@
 """MoE operator package."""
 
+from .abc import (
+    MoEExperts,
+    MoEExpertsModular,
+    MoEPrepareAndFinalize,
+    PrepareResult,
+    WeightedReduce,
+    WeightedReduceNoOp,
+)
+from .experts.nopad import MoEExpertsNopad
+from .experts.padded import MoEExpertsPadded
 from .fused_moe import FusedMoe
 from .fused_moe_experts import FusedMoeExpertsFwdOp, FusedMoeExpertsPaddedFwdOp
 from .fused_topk import FusedTopKOp
@@ -7,10 +17,23 @@ from .moe_grouped_gemm_nopad import MoeGroupedGemmNopadFwdOp
 from .permute_align import MoePermuteAlignFwdOp
 from .permute_nopad import MoePermuteNopadFwdOp
 from .permute_padded import MoePermutePaddedFwdOp
+from .prepare_finalize.no_dp_ep import MoEPrepareAndFinalizeNoDPEP
 from .shared_fused_moe import SharedFusedMoE
 from .unpermute import MoeUnpermuteFwdOp
 
 __all__ = [
+    # ABCs and data structures
+    "MoEExperts",
+    "MoEExpertsModular",
+    "MoEPrepareAndFinalize",
+    "PrepareResult",
+    "WeightedReduce",
+    "WeightedReduceNoOp",
+    # Concrete implementations
+    "MoEExpertsNopad",
+    "MoEExpertsPadded",
+    "MoEPrepareAndFinalizeNoDPEP",
+    # Existing ops (kept for backwards compatibility)
     "FusedMoe",
     "FusedMoeExpertsFwdOp",
     "FusedMoeExpertsPaddedFwdOp",
