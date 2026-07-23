24a82a3d2d3f6149dfb7138e0fc7320db0f33e93
jienengyu <jieneng.yu@aigcic.com>
2026-07-23 22:14:40 +0800

    [Refactor][MoE] Separate dispatched expert compute

--- src/tileops/ops/moe/__init__.py
+++ src/tileops/ops/moe/__init__.py
@@ -1,36 +1,41 @@
 """MoE operator package."""
 
-from .abc import (
-    FusedMoEExperts,
-    FusedMoEExpertsModular,
-    FusedMoEPrepareAndFinalize,
-    PrepareResult,
-    WeightedReduce,
-    WeightedReduceNoOp,
-)
-from .fused_moe import FusedMoe, FusedMoeFwdOp
+from .fused_moe import FusedMoe, FusedMoeFwdCbFwdOp, FusedMoeFwdOp
 from .fused_topk import FusedTopKOp
 from .permute_align import MoePermuteAlignFwdOp
 from .prepare_finalize.no_dp_ep import MoEPrepareAndFinalizeNoDPEP
 from .routed_expert import (
+    DispatchedExpertMLPFwdOp,
+    ExpertBatch,
+    ExpertBatchOutput,
+    FusedMoEExperts,
+    FusedMoEExpertsModular,
     FusedMoEExpertsNopadPersistent3WGFwdOp,
-    MoeGateUpFwdOp,
+    MoeGroupedGemmNopad3WGFusedActFwdOp,
     MoeGroupedGemmNopadFwdOp,
     MoePermuteNopadFwdOp,
     MoeUnpermuteFwdOp,
+    PrepareResult,
+    WeightedReduce,
+    WeightedReduceNoOp,
 )
+from .routed_expert.abc import FusedMoEPrepareAndFinalize
 from .shared_fused_moe import SharedFusedMoE
 
 __all__ = [
+    "DispatchedExpertMLPFwdOp",
+    "ExpertBatch",
+    "ExpertBatchOutput",
     "FusedMoEExperts",
     "FusedMoEExpertsModular",
     "FusedMoEExpertsNopadPersistent3WGFwdOp",
     "FusedMoEPrepareAndFinalize",
     "FusedMoe",
+    "FusedMoeFwdCbFwdOp",
     "FusedMoeFwdOp",
     "FusedTopKOp",
     "MoEPrepareAndFinalizeNoDPEP",
-    "MoeGateUpFwdOp",
+    "MoeGroupedGemmNopad3WGFusedActFwdOp",
     "MoeGroupedGemmNopadFwdOp",
     "MoePermuteAlignFwdOp",
     "MoePermuteNopadFwdOp",
--- src/tileops/ops/moe/routed_expert/__init__.py
+++ src/tileops/ops/moe/routed_expert/__init__.py
@@ -1,17 +1,35 @@
 """Routed expert implementations and supporting operations."""
 
+from .abc import (
+    ExpertBatch,
+    ExpertBatchOutput,
+    FusedMoEExperts,
+    FusedMoEExpertsModular,
+    PrepareResult,
+    WeightedReduce,
+    WeightedReduceNoOp,
+)
+from .dispatched_expert import DispatchedExpertMLPFwdOp
 from .fused_routed_expert import (
     FusedMoEExpertsNopadPersistent3WGFwdOp,
 )
-from .gate_up import MoeGateUpFwdOp
 from .moe_grouped_gemm_nopad import MoeGroupedGemmNopadFwdOp
+from .moe_grouped_gemm_nopad_fused_act import MoeGroupedGemmNopad3WGFusedActFwdOp
 from .permute_nopad import MoePermuteNopadFwdOp
 from .unpermute import MoeUnpermuteFwdOp
 
 __all__ = [
+    "DispatchedExpertMLPFwdOp",
+    "ExpertBatch",
+    "ExpertBatchOutput",
+    "FusedMoEExperts",
+    "FusedMoEExpertsModular",
     "FusedMoEExpertsNopadPersistent3WGFwdOp",
-    "MoeGateUpFwdOp",
+    "MoeGroupedGemmNopad3WGFusedActFwdOp",
     "MoeGroupedGemmNopadFwdOp",
     "MoePermuteNopadFwdOp",
     "MoeUnpermuteFwdOp",
+    "PrepareResult",
+    "WeightedReduce",
+    "WeightedReduceNoOp",
 ]
