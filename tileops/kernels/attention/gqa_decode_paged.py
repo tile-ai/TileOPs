eced022d1ed8d9fe615d5b4fc0bb9cfab567f70b
SuperAngGao <gaoang0125@163.com>
2026-07-27 10:23:32 +0800

    Refactor paged GQA dispatch change scope

--- tileops/kernels/attention/gqa_decode_paged.py
+++ tileops/kernels/attention/gqa_decode_paged.py
@@ -135,6 +135,9 @@ def _gqa_decode_no_split_paged_kernel(
                     online_softmax(acc_s, scores_max, scores_max_prev, scores_scale, scores_sum, logsum)
                     T.copy(acc_s, acc_s_cast)
                     rescale(acc_o, scores_scale)
+                    T.copy(
+                        V[blockn_num_offset * block_N:(blockn_num_offset + 1) * block_N,
+                          cur_kv_head, :], V_shared)
                     T.gemm(acc_s_cast, V_shared, acc_o, policy=T.GemmWarpPolicy.FullRow)
                 for i, j in T.Parallel(block_H, dim):
                     acc_o[i, j] = T.if_then_else(logsum[i] == 0, 0, acc_o[i, j] / logsum[i])
@@ -276,6 +279,9 @@ def _gqa_decode_split_paged_kernel(
                     online_softmax(acc_s, scores_max, scores_max_prev, scores_scale, scores_sum, logsum)
                     T.copy(acc_s, acc_s_cast)
                     rescale(acc_o, scores_scale)
+                    T.copy(
+                        V[blockn_num_offset * block_N:(blockn_num_offset + 1) * block_N,
+                          cur_kv_head, :], V_shared)
                     T.gemm(acc_s_cast, V_shared, acc_o, policy=T.GemmWarpPolicy.FullRow)
                 for i, j in T.Parallel(block_H, dim):
                     # When loop_range was 0 (split entirely beyond real_seqlen_kv), logsum=0 -> avoid 0/0
